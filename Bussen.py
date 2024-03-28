abd917d14388e0f13cbccbbbd7700916b8ddfc07
Truxi <152483949+PrgVik@users.noreply.github.com>
2024-03-28 10:32:40 +0100

    Update Bussen.py

--- Bussen.py
+++ Bussen.py
@@ -1,8 +1,8 @@
 import random as rand
 
 class Passagerare:
-    def __init__(self, person, ålder, kön):
-        self.person = person
+    def __init__(self, namn, ålder, kön):
+        self.namn = person
         self.ålder = ålder
         self.kön = kön
 
