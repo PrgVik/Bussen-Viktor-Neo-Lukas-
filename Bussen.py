bee996051d918a3ada7c0b3c8188b456f03d3014
Truxi <152483949+PrgVik@users.noreply.github.com>
2024-04-11 10:34:58 +0200

    Update Bussen.py

--- Bussen.py
+++ Bussen.py
@@ -6,5 +6,30 @@ class Passagerare:
         self.ålder = ålder
         self.kön = kön
 
+person_1 = Person("", "", "")
+person_2 = Person("", "", "")
+person_3 = Person("", "", "")
+person_4 = Person("", "", "")
+person_5 = Person("", "", "")
+person_6 = Person("", "", "")
+person_7 = Person("", "", "")
+person_8 = Person("", "", "")
+person_9 = Person("", "", "")
+person_10 = Person("", "", "")
+person_11 = Person("", "", "")
+person_12 = Person("", "", "")
+person_13 = Person("", "", "")
+person_14 = Person("", "", "")
+person_15 = Person("", "", "")
+person_16 = Person("", "", "")
+person_17 = Person("", "", "")
+person_18 = Person("", "", "")
+person_19 = Person("", "", "")
+person_20 = Person("", "", "")
+person_21 = Person("", "", "")
+person_22 = Person("", "", "")
+person_23 = Person("", "", "")
+person_24 = Person("", "", "")
+person_25 = Person("", "", "")
 
 
